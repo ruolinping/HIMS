86151d054de24e174b07b5549d6f7debe1facede
R <R@RPING>
2016-10-25 19:31:11 -0400

    inital

---
+++ HospitalInformationManagementSystem/AngularJsApp/mainApp.js
@@ -0,0 +1,13 @@
+﻿var mainApp = angular.module('mainApp', ['ui.router']);
+
+mainApp..config(function($stateProvider, $urlRouterProvider) {
+
+});
+
+
+
+mainApp.controller('mainController', function ($scope) {
+
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
