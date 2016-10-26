163f7c6ecbd3e84ffc32a616a0db1963eef0e0c5
R <R@RPING>
2016-10-25 20:10:18 -0400

    initial2 with ui router

--- HospitalInformationManagementSystem/AngularJsApp/mainApp.js
+++ HospitalInformationManagementSystem/AngularJsApp/mainApp.js
@@ -1,7 +1,16 @@
 ﻿var mainApp = angular.module('mainApp', ['ui.router']);
 
-mainApp..config(function($stateProvider, $urlRouterProvider) {
+mainApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/Home');
+    $stateProvider.state('Home', {
+        url: '/Home',
+        templateUrl: 'home.html'
+    })
 
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
 });
 
 
